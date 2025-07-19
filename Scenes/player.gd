2483b56e36ce16c74d5614ee58c907e0c4381caf
yoobezxc <yoobedev@gmail.com>
2025-07-19 07:54:54 +0700

    experiment using lerp on player movement

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -3,31 +3,35 @@ extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY: float = 90.0
+const SPEED: float = 50.0
+const JUMP: float = -60.0
 
 func _ready() -> void:
 	sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
+	print(self.velocity)
+	
 	if Input.is_action_pressed("RIGHT"):
-		self.velocity.x += 50.0 * delta
+		self.velocity.x += SPEED * delta
 		sprite.flip_h = false
 		
 		if is_on_floor():
 			sprite.play("run")
 	elif Input.is_action_pressed("LEFT"):
-		self.velocity.x -= 50.0 * delta
+		self.velocity.x -= SPEED * delta
 		sprite.flip_h = true
 		
 		if is_on_floor():
 			sprite.play("run")
 	else:
-		self.velocity.x = 0.0
+		self.velocity.x = lerp(self.velocity.x, 0.0, easeOutSine(0.2))
 		
 		if is_on_floor():
 			sprite.play("idle")
 	
 	if Input.is_action_pressed("SPACE") and is_on_floor():
-		self.velocity.y = -60.0
+		self.velocity.y = JUMP
 		
 		if self.velocity.y < 0:
 			sprite.play("start_jump")
@@ -39,3 +43,6 @@ func _physics_process(delta: float) -> void:
 			sprite.play("fall_jump")
 	
 	move_and_slide()
+
+func easeOutSine(x: float) -> float:
+	return sin((x * PI) / 2)
