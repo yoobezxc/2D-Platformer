5c0f3f60574168c1ec36ade7f0d337d95be38b1e
yoobezxc <yoobedev@gmail.com>
2025-07-19 07:28:37 +0700

    Adding animation control and little tweak on player

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -11,19 +11,31 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("RIGHT"):
 		self.velocity.x += 50.0 * delta
 		sprite.flip_h = false
-		sprite.play("run")
+		
+		if is_on_floor():
+			sprite.play("run")
 	elif Input.is_action_pressed("LEFT"):
 		self.velocity.x -= 50.0 * delta
 		sprite.flip_h = true
-		sprite.play("run")
-	elif Input.is_action_pressed("SPACE") and is_on_floor():
-		self.velocity.y -= 2000.0 * delta
-		sprite.play("start_jump")
+		
+		if is_on_floor():
+			sprite.play("run")
 	else:
 		self.velocity.x = 0.0
-		sprite.play("idle")
+		
+		if is_on_floor():
+			sprite.play("idle")
+	
+	if Input.is_action_pressed("SPACE") and is_on_floor():
+		self.velocity.y = -60.0
+		
+		if self.velocity.y < 0:
+			sprite.play("start_jump")
 	
 	if not is_on_floor():
 		self.velocity.y += GRAVITY * delta
+		
+		if self.velocity.y > 0:
+			sprite.play("fall_jump")
 	
 	move_and_slide()
