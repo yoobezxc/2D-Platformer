81c15b7eba25fddb52a38c483689e72bee00ef5e
yoobezxc <yoobedev@gmail.com>
2025-07-21 19:58:38 +0700

    Better player movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,13 +3,15 @@ extends CharacterBody2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY: float = 90.0
-const SPEED: float = 20.0
-const JUMP: float = -50.0
+const SPEED: float = 40.0
+const JUMP: float = -60.0
 
 func _ready() -> void:
 	sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
+	print(velocity)
+	
 	if not is_on_floor():
 		self.velocity.y += GRAVITY * delta
 		
@@ -17,30 +19,27 @@ func _physics_process(delta: float) -> void:
 			sprite.play("fall_jump")
 	
 	if Input.is_action_pressed("RIGHT"):
-		self.velocity.x += SPEED * delta
+		self.velocity.x = max(SPEED * delta, 60.0)
 		sprite.flip_h = false
 		
 		if is_on_floor():
 			sprite.play("run")
 	elif Input.is_action_pressed("LEFT"):
-		self.velocity.x -= SPEED * delta
+		self.velocity.x = min(-SPEED * delta, -60.0)
 		sprite.flip_h = true
 		
 		if is_on_floor():
 			sprite.play("run")
 	else:
-		self.velocity.x = lerp(self.velocity.x, 0.0, 0.2)
+		self.velocity.x = 0.0
 		
 		if is_on_floor():
 			sprite.play("idle")
 	
-	if Input.is_action_pressed("SPACE") and is_on_floor():
+	if Input.is_action_just_pressed("SPACE") and is_on_floor():
 		self.velocity.y = JUMP
 		
 		if self.velocity.y < 0:
 			sprite.play("start_jump")
 	
 	move_and_slide()
-
-func easeOutSine(x: float) -> float:
-	return sin((x * PI) / 2)
