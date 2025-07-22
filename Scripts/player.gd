4253f9d516b73f34804c3be99148181310d7bd7f
yoobezxc <yoobedev@gmail.com>
2025-07-22 15:20:32 +0700

    Improve movement and jump player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,28 +4,38 @@ extends CharacterBody2D
 
 const GRAVITY: float = 90.0
 const SPEED: float = 40.0
-const JUMP: float = -60.0
+const JUMP: float = -50.0
+const JUMP_DELAY: float = 0.3
+
+var is_jumping: bool
+var time_elapsed: float
 
 func _ready() -> void:
 	sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
 	print(velocity)
+	print(time_elapsed)               
 	
 	if not is_on_floor():
 		self.velocity.y += GRAVITY * delta
+		time_elapsed += delta
 		
 		if self.velocity.y > 0:
 			sprite.play("fall_jump")
 	
+	if is_on_floor():
+		is_jumping = false
+		time_elapsed = 0.0
+	
 	if Input.is_action_pressed("RIGHT"):
-		self.velocity.x = max(SPEED * delta, 60.0)
+		self.velocity.x = SPEED
 		sprite.flip_h = false
 		
 		if is_on_floor():
 			sprite.play("run")
 	elif Input.is_action_pressed("LEFT"):
-		self.velocity.x = min(-SPEED * delta, -60.0)
+		self.velocity.x = -SPEED
 		sprite.flip_h = true
 		
 		if is_on_floor():
@@ -36,8 +46,9 @@ func _physics_process(delta: float) -> void:
 		if is_on_floor():
 			sprite.play("idle")
 	
-	if Input.is_action_just_pressed("SPACE") and is_on_floor():
+	if Input.is_action_just_pressed("SPACE") and not is_jumping and time_elapsed <= JUMP_DELAY:
 		self.velocity.y = JUMP
+		is_jumping = true
 		
 		if self.velocity.y < 0:
 			sprite.play("start_jump")
